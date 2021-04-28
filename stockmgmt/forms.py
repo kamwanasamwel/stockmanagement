8c45bc73892eead081ef3e8d6e0b092f9c02828d
kamwanasamwel <kamwanasamwel@gmail.com>
2021-04-28 17:07:42 +0300

    added stockupdate form

--- stockmgmt/forms.py
+++ stockmgmt/forms.py
@@ -28,3 +28,9 @@ class StockSearchForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['category', 'item_name']
+
+
+class StockUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['category', 'item_name', 'quantity']
